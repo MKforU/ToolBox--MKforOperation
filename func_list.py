00a5958d6c46fcea3e3f663f1cd33efcdf870d59
懒羊羊大王万岁！ <734376734@qq.com>
2026-03-16 15:05:06 +0800

    Update func_list.py

--- func_list.py
+++ func_list.py
@@ -1,9 +1,13 @@
-# func_list.py  汇总所有功能
 from functions.pdf_to_image import run as pdf_to_image
+from functions.remove_duplicate import run as remove_duplicate
 
 FUNCTION_LIST = {
     "1": {
         "name": "PDF 转 图片",
         "func": pdf_to_image
+    },
+    "2": {
+        "name": "表格去重",
+        "func": remove_duplicate
     }
 }
